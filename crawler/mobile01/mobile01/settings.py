720b79382d86b44e7f5c26547266e90a7925deb2
bryanyang0528 <kenshin2004528@hotmail.com>
2014-12-04 17:26:12 +0800

    init

---
+++ crawler/mobile01/mobile01/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for mobile01 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'mobile01'
+
+SPIDER_MODULES = ['mobile01.spiders']
+NEWSPIDER_MODULE = 'mobile01.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'mobile01 (+http://www.yourdomain.com)'
